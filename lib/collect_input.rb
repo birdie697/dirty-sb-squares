8b95fde20ff8f906c3677e2d288d41e41bd62ef5
Marc Marino <marc.a.marino@gmail.com>
2019-01-30 21:05:01 -0500

    write to a file as well as console

--- lib/collect_input.rb
+++ lib/collect_input.rb
@@ -1,22 +1,32 @@
+require_relative "disperse_output"
+require_relative "constants"
+
+
 module Collect_input
 
   def get_name
-    print "Please enter a name:  "
+    text_string = "Please enter a name:  "
+    print "#{text_string}"
     name = gets.chomp
+    Disperse_output.open_file_append("#{text_string}#{name}")
     return name
   end
   module_function :get_name
 
   def get_squares_count
-    print "Please enter how many squares you want:  "
+    text_string = "Please enter how many squares you want:  "
+    print "#{text_string}"
     squares_count = gets.chomp.to_i
+    Disperse_output.open_file_append("#{text_string}#{squares_count}\n")
     return squares_count.to_i
   end
   module_function :get_squares_count
 
   def try_again
-    puts "You cannot purchase that many squares!  Try again."
-    squares_count = Constants.get_squares_count
+    text_string = "You cannot purchase that many squares!  Try again."
+    puts "#{text_string}"
+    Disperse_output.open_file_append("#{text_string}")
+    squares_count = Collect_input.get_squares_count
     return squares_count
   end
   module_function :try_again
--- lib/disperse_output.rb
+++ lib/disperse_output.rb
@@ -1,8 +1,31 @@
 module Disperse_output
 
+  def how_to_play(current_time)
+    output  = "Greetings and salutations!\n"
+    output += "The current time is:  #{current_time}\n"
+    output += "\n"
+    output += "You will be prompted to enter a name.\n"
+    output += "You will be prompted to enter the number of squares that you want\n"
+    output += "This will repeat until all squares of the 10x10 grid have been\n"
+    output += "assigned to a name.\n"
+    output += "\n"
+    output += "The New England Patriots (NE) scores are across the top.\n"
+    output += "The Los Angeles Rams (LA) scores are along the side\n"
+    output += "\n"
+    output += "There is a winner for each quarter.  Take the ones digit of the\n"
+    output += "Patriots score across the top, pair it with the ones digit of the\n"
+    output += "Rams score, locate the name in that cell and that is the winner!\n"
+    output += "\n"
+    output += "Good luck! Beat LA!\n"
+    output += "\n"
+    return output
+  end
+  module_function :how_to_play
+
   def available_squares(count)
     output = "The count for available squares is now #{count}\n"
     output += "\n"
+    open_file_append(output)
     return output
   end
   module_function :available_squares
@@ -18,9 +41,31 @@ module Disperse_output
     when 3
       output = "Q1".ljust(4) + "Q2".ljust(4) + "Q3".ljust(4) + "Q4".ljust(4)
     end
+    open_file_print_append(output)
     return output
   end
   module_function :row_and_column_headers
+
+  def open_file_write(text)
+    File.open("#{Constants::FILE_NAME}", "w") do |file|
+      file.puts "#{text}"
+    end
+  end
+  module_function :open_file_write
+
+  def open_file_append(text)
+    File.open("#{Constants::FILE_NAME}", "a") do |file|
+      file.puts "#{text}"
+    end
+  end
+  module_function :open_file_append
+
+  def open_file_print_append(text)
+    File.open("#{Constants::FILE_NAME}", "a") do |file|
+      file.print "#{text}"
+    end
+  end
+  module_function :open_file_print_append
 
 
 end
